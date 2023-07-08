595991a16f43924b58f977224504c47e72764516
single-byte <70197335+single-byte@users.noreply.github.com>
2023-07-08 12:46:11 +0500

    added gitignore

--- test.js
+++
@@ -1,6 +0,0 @@
-import extractTextFromImage from "textifyimage";
-
-// returns a promise
-var test = extractTextFromImage("test.jpg");
-// callback for the promise
-test.then((data) => console.log(data)).catch((error) => console.log(error));
