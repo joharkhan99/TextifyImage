5ac57c8957811808e3392ee54b9b02c2a7e00ae6
single-byte <70197335+single-byte@users.noreply.github.com>
2023-07-12 16:08:57 +0500

    added Text to Image Feature

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { createWorker } from "tesseract.js";
+import { createCanvas } from "canvas";
+import fs from "fs";
 
 const worker = await createWorker();
 
@@ -12,4 +14,62 @@ var extractTextFromImage = async (imagePath) => {
   return text;
 };
 
-export default extractTextFromImage;
+function convertTextToImage({
+  text,
+  outputPath,
+  fontSize = "16px",
+  fontName = "Arial",
+  textColor = "black",
+  backgroundColor = "white",
+  width = 400,
+  height = 200,
+  lineHeight = 24,
+  textAlignment = "center",
+  x = null,
+  y = null,
+}) {
+  const canvas = createCanvas(width, height);
+  const context = canvas.getContext("2d");
+
+  context.fillStyle = backgroundColor;
+  context.fillRect(0, 0, width, height);
+
+  // registerFont('path/to/font.ttf', { family: fontName });
+
+  context.font = `${fontSize} ${fontName}`;
+  context.fillStyle = textColor;
+  context.textAlign = textAlignment;
+
+  const words = text.split(" ");
+  let line = "";
+  let yPos = y !== null ? y : height / 2 - (lineHeight * words.length) / 2;
+  let xPos = x !== null ? x : width / 2;
+
+  for (let i = 0; i < words.length; i++) {
+    const testLine = line + words[i] + " ";
+    const metrics = context.measureText(testLine);
+    const testWidth = metrics.width;
+
+    if (testWidth > width && i > 0) {
+      context.fillText(line.trim(), xPos, yPos);
+      line = words[i] + " ";
+      yPos += lineHeight;
+    } else {
+      line = testLine;
+      xPos = (width - metrics.width) / 2;
+    }
+  }
+
+  context.fillText(line.trim(), xPos, yPos);
+
+  return new Promise((resolve, reject) => {
+    const writeStream = fs.createWriteStream(outputPath);
+    const stream = canvas.createPNGStream();
+
+    stream.pipe(writeStream);
+    writeStream.on("finish", () => resolve());
+    writeStream.on("error", (error) => reject(error));
+  });
+}
+
+export { extractTextFromImage, convertTextToImage };
--- test.js
+++ test.js
@@ -1,6 +1,33 @@
-import extractTextFromImage from "textifyimage";
+import { convertTextToImage, extractTextFromImage } from "textifyimage";
 
+// IMAGE TO TEXT
 // returns a promise
 var test = extractTextFromImage("test.jpg");
 // callback for the promise
 test.then((data) => console.log(data)).catch((error) => console.log(error));
+
+// TEXT TO IMAGE
+const text = "Hello, World Text!";
+const outputPath = "output.png";
+
+// returns a callback. handle it with then and catch
+convertTextToImage({
+  text,
+  outputPath,
+  fontSize: "30px",
+  fontName: "Arial",
+  textColor: "rgb(0,0,0)",
+  backgroundColor: "orange",
+  width: 200,
+  height: 200,
+  lineHeight: 30,
+  textAlignment: "start",
+  x: 0,
+  y: 30,
+})
+  .then(() => {
+    console.log(`Image saved to ${outputPath}`);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
